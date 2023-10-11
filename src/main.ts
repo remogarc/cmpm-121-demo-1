81c13e09a9baa2923d85e87796f7f1d726ce3e2e
Reese Garcia <remogarc@ucsc.edu>
2023-10-10 19:18:13 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,12 @@ header.innerHTML = gameName;
 app.append(header);
 
 function setupCounter(element: HTMLButtonElement) {
-    let counter = 0
+  let counter = 0;
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `🎃 ${counter} Pumpkins`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `🎃 ${counter} Pumpkins`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
 }
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
